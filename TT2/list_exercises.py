7789498b13abca2595d4d96f99ecd25cd211e20f
ZhaoWang-UC <88282524+ZhaoWang-UC@users.noreply.github.com>
2023-10-17 20:46:20 -0500

    Add files via upload

--- TT2/list_exercises.py
+++ TT2/list_exercises.py
@@ -1,15 +1,10 @@
-"""
-CS121 Team Tutorial 2: Functions
-"""
+# CS121 Team Tutorial 2: Functions
+
+# Write functions are_any_true, add_lists, and add_one
 
 def count_twos(lst):
     """
     Count the number of twos in a list
-
-    Input:
-       lst (list): the list
-
-    Returns (int): The number of twos in lst
     """
     count = 0
     for item in lst:
@@ -17,15 +12,28 @@ def count_twos(lst):
             count = count + 1
     return count
 
-# Add your are_any_true function here
+def are_any_true(l):
+    for v in l:
+        if v:
+            return True
+        
+    return False
 
-# Add your add_lists function here
+def add_lists(a, b):
+    assert len(a) == len(b), "Lists must be the same length"
+    c = []
+    for i in range(len(a)):
+        c.append(a[i] + b[i])
 
-# Add your add_one function here
+    return c
+                
+def add_one(a):
+    for i in range(len(a)):
+        a[i] = a[i] + 1
 
 def go():
     '''
-    Write code to verify that your functions work as expected here.
+    Write code to verify that your functions work as expected here
     Try to think of a few good examples to test your work.
     '''
 
@@ -34,7 +42,25 @@ def go():
     print(count_twos(lst1))
     print(count_twos(lst2))
 
-    # Add your tests here
+    test_list = [True, True, False, True, True]
+    test_list1 = [True, True, False, True, False]
+    test_list2 = [False, False, False]
+
+    print(are_any_true(test_list))
+    print(are_any_true(test_list1))
+    print(are_any_true(test_list2))
+    print(are_any_true([]))
+
+    a = [1, 2, 3, 4, 5]
+    b = [2, 2, 2, 2, 2]
+
+    print(add_lists(a, b))
+
+    print(add_lists([], []))
+
+    add_one(a)
+    print(a)
 
 if __name__ == "__main__":
     go()
+    
--- TT2/min_max.py
+++ TT2/min_max.py
@@ -1,29 +1,48 @@
-"""
-Abstraction practice for Team Tutorial 2 (Functions)
-"""
-
-def compute_frequencies(lst):
+def compute_frequencies(lst, debug=False):
     """
     Count how often each value between 0 and M (the maximum
     value in the list) occurs in the input list.
 
     Inputs:
-      lst (list of ints): list of integers between 0 and some upper bound M
+      lst: list of integers between 0 and some upper bound M
         (inclusive), where M is expected to be relative small (say,
         less than 1000).
 
-    Returns (list of ints): list where the ith element is the number of times
+    Returns: list where the ith element is the number of times
      i occurs in lst.
     """
 
     # allocate space to hold the lst
     frequencies = [0] * (max(lst) + 1)
+    if debug:
+        print("Initial value of frequencies:", frequencies)
 
     for val in lst:
         frequencies[val] = frequencies[val] + 1
+        if debug:
+            print("Val is: {}... incrementing frequencies[{}]".format(val, val))
 
     return frequencies
 
+
+def find_matching_indices(lst, target_val):
+    """
+    Return a list of the indices that match val
+
+    Inputs:
+       lst: a list
+       target_val: a value
+
+    Returns: a list of ints
+    """
+
+    # Find all the values that match val
+    rv = []
+    for i, val in enumerate(lst):
+        if val == target_val:
+            rv.append(i)
+    return rv
+    
 
 def find_most_frequent_values(lst):
     """
@@ -31,20 +50,36 @@ def find_most_frequent_values(lst):
     frequently in the list.
 
     Inputs:
-      lst (list of ints): list of integers between 0 and some upper bound M
+      lst: list of integers between 0 and some upper bound M
         (inclusive), where M is expected to be relative small
         (say, less than 1000).
 
-    Returns (list of ints): list of the int(s) that occur most frequently.
+    Returns: list of the int(s) that occur most frequently.
     """
 
-    # Determine how frequently most frequent value(s) occurs.
+    # Determine how frequently the most frequent value(s) occurs.
     frequencies = compute_frequencies(lst)
     max_freq = max(frequencies)
 
-    # Find all the values that occur max_freq times.
-    rv = []
-    for i, freq in enumerate(frequencies):
-        if freq == max_freq:
-            rv.append(i)
-    return rv
+    return find_matching_indices(frequencies, max_freq)
+
+
+def find_least_frequent_values(lst):
+    """
+    Find the value or values (in the case of ties) that occur most
+    frequently in the list.
+
+    Inputs:
+      lst: list of integers between 0 and some upper bound M
+        (inclusive), where M is expected to be relative small
+        (say, less than 1000).
+
+    Returns: list of the int(s) that occur most frequently.
+    """
+
+    # Determine how frequently the least frequent value(s) occurs.
+    frequencies = compute_frequencies(lst)
+    min_freq = min([v for v in frequencies if v > 0])
+
+    return find_matching_indices(frequencies, min_freq)
+
